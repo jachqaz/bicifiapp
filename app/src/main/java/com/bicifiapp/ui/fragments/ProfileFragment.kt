73ec93258537016e77197e7632e9dbf67a176518
Jacob Cardozo <jaccobcl@gmail.com>
2020-04-01 20:36:10 -0500

    se adiciona layout perfil

--- app/src/main/java/com/bicifiapp/ui/fragments/ProfileFragment.kt
+++ app/src/main/java/com/bicifiapp/ui/fragments/ProfileFragment.kt
@@ -36,7 +36,7 @@ class NotificationPassFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_notification_pass, container, false)
+        return inflater.inflate(R.layout.fragment_profile, container, false)
     }
 
     companion object {
